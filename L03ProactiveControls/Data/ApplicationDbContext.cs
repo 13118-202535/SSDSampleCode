29853ae56fed9f22e8e12ab7c1a35acf29672c87
Mohamed Elliethy <mohamed.elliethy@mohawkcollege.ca>
2025-10-21 23:19:27 -0400

    Proactive Controls - Part 1

---
+++ L03ProactiveControls/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using L03ProactiveControls.Models;
+
+namespace L03ProactiveControls.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<L03ProactiveControls.Models.Customer> Customer { get; set; } = default!;
+    }
+}
---
+++ L03ProactiveControls/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L03ProactiveControls.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
